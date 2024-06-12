0ef3117097d42f30685cb2fc9a2cdf8cb3d7dff9
DilyorbekUbaydullayev <dilyorbekubaydullayev@gmail.com>
2024-06-12 16:08:42 +0500

    finish UI3

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,7 @@
-import { Routes,Route } from "react-router-dom";
 import "./app.css";
-import Navbar from "./components/navbar/navbar";
-import Home from "./pages/home/home";
-import About from "./pages/about/about";
-import Work from "./pages/work/work";
-import Pricing from "./pages/pricing/pricing";
-import Contact from "./pages/contact/contact";
-import Footer from "./components/footer/footer";
-
 function App() {
   return (
     <div className="App">
-      <Navbar />
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/About" element={<About />} />
-    <Route path="/Work" element={<Work />} />
-    <Route path="/Pricing" element={<Pricing />} />
-    <Route path="/Contact" element={<Contact />} />
-    
-  </Routes>
-  <Footer></Footer>
     </div>
   );
 }
